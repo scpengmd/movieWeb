e126fa6c6ffe5b888ce3ced67631b90e393449b1
scpengmd <pengmd@shuchidt.com>
2018-03-11 19:45:12 +0800

    5-3 电影管理-上映预告管理页面搭建

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 from . import admin
-from flask import render_template,redirect,url_for
+from flask import render_template, redirect, url_for
 
 
 @admin.route("/")
@@ -22,10 +22,32 @@ def logout():
 def pwd():
     return render_template("admin/pwd.html")
 
+
 @admin.route("/tag/add/")
 def tag_add():
     return render_template("admin/tag_add.html")
 
+
 @admin.route("/tag/list/")
 def tag_list():
     return render_template("admin/tag_list.html")
+
+
+@admin.route("/movie/add/")
+def movie_add():
+    return render_template("admin/movie_add.html")
+
+
+@admin.route("/movie/list/")
+def movie_list():
+    return render_template("admin/movie_list.html")
+
+
+@admin.route("/preview/add/")
+def preview_add():
+    return render_template("admin/preview_add.html")
+
+
+@admin.route("/preview/list/")
+def preview_list():
+    return render_template("admin/preview_list.html")
